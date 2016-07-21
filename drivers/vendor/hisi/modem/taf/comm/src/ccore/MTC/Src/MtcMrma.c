a14e80b445fb53fca2e98de48c4def6ef7697b40
Kevindeving <98kevchen@gmail.com>
2016-07-21 18:35:56 +0200

    Mate S Open Source (CRR-UL00&L09, Android 5.1.1, kernel)

---
+++ drivers/vendor/hisi/modem/taf/comm/src/ccore/MTC/Src/MtcMrma.c
@@ -0,0 +1,106 @@
+
+
+/*****************************************************************************
+  1 头文件包含
+*****************************************************************************/
+#include "MtcRcmInterface.h"
+#include "ImsaMtcInterface.h"
+#include "MtcCtx.h"
+#include "MtcDebug.h"
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif
+
+#define    THIS_FILE_ID        PS_FILE_ID_MTC_MRMA_C
+
+/*****************************************************************************
+  2 全局变量定义
+*****************************************************************************/
+
+/*****************************************************************************
+  3 函数定义
+*****************************************************************************/
+
+VOS_VOID MTC_SndRcmModemStateInd(VOS_VOID)
+{
+    MTC_RCM_MODEM_STATE_NOTIFY_STRU    *pstModemState;
+    MODEM_ID_ENUM_UINT16                enModemId;
+
+    /* 申请消息包 */
+    pstModemState = (MTC_RCM_MODEM_STATE_NOTIFY_STRU *)MTC_ALLOC_MSG_WITH_HDR(sizeof(MTC_RCM_MODEM_STATE_NOTIFY_STRU));
+    if (VOS_NULL_PTR == pstModemState)
+    {
+        MTC_ERROR_LOG("MTC_SndRcmModemStateInd: Alloc Msg Fail!");
+        return;
+    }
+
+    PS_MEM_SET(((VOS_UINT8*)pstModemState) + VOS_MSG_HEAD_LENGTH, 0, sizeof(MTC_RCM_MODEM_STATE_NOTIFY_STRU) - VOS_MSG_HEAD_LENGTH);
+
+    /* 填充消息头 */
+    MTC_CFG_RCM_MSG_HDR(&pstModemState->stMsgHeader, ID_MTC_RCM_MODEM_STATUS_NOTIFY);
+
+    /* 填写modem0和modem1的状态信息 */
+    for (enModemId = 0; enModemId < MODEM_ID_BUTT ; enModemId++)
+    {
+        pstModemState->astModemState[enModemId].enCsSrvConnState        = MTC_GetCsConnStateInfo(enModemId);
+        pstModemState->astModemState[enModemId].enPsSrvConnState        = MTC_GetRcmPsConnStateInfo(enModemId);
+        pstModemState->astModemState[enModemId].enModemPowerState       = MTC_GetModemPowerState(enModemId);
+        pstModemState->astModemState[enModemId].enUsimmCardState        = MTC_GetUsimmStateInfo(enModemId);
+        pstModemState->astModemState[enModemId].enModemType             = MTC_RCM_MODEM_0 + enModemId;
+    }
+
+    /* 填写CDMA状态信息 */
+    pstModemState->astModemState[MTC_RCM_MODEM_CDMA].enCsSrvConnState   = MTC_GetOutSideCsConnStateInfo(MTC_OUTSIDE_MODEM_CDMA);
+    pstModemState->astModemState[MTC_RCM_MODEM_CDMA].enPsSrvConnState   = MTC_GetOutSidePsConnStateInfo(MTC_OUTSIDE_MODEM_CDMA);
+    pstModemState->astModemState[MTC_RCM_MODEM_CDMA].enModemPowerState  = MTC_GetOutSideModemPowerState(MTC_OUTSIDE_MODEM_CDMA);
+    pstModemState->astModemState[MTC_RCM_MODEM_CDMA].enUsimmCardState   = MTC_GetOutSideModemUsimmState(MTC_OUTSIDE_MODEM_CDMA);
+    pstModemState->astModemState[MTC_RCM_MODEM_CDMA].enModemType        = MTC_RCM_MODEM_CDMA;
+
+    /* 发送消息 */
+    MTC_SEND_MSG(pstModemState);
+
+    return;
+}
+VOS_VOID MTC_SndImsaModem1InfoInd(VOS_VOID)
+{
+    MTC_IMSA_MODEM1_INFO_IND_STRU      *pstModem1Info = VOS_NULL_PTR;
+
+    /* IMSA没有Start则不需要发消息 */
+    if (MTC_MODEM_POWER_ON != MTC_GetModemImsaState(MODEM_ID_0))
+    {
+        return;
+    }
+
+    /* 申请消息包 */
+    pstModem1Info = (MTC_IMSA_MODEM1_INFO_IND_STRU *)MTC_ALLOC_MSG_WITH_HDR(sizeof(MTC_IMSA_MODEM1_INFO_IND_STRU));
+    if (VOS_NULL_PTR == pstModem1Info)
+    {
+        MTC_ERROR_LOG("MTC_SndImsaModem1InfoInd: Alloc Msg Fail!");
+        return;
+    }
+
+    /* 清空消息内容 */
+    PS_MEM_SET(MTC_GET_MSG_ENTITY(pstModem1Info), 0x00, MTC_GET_MSG_LENGTH(pstModem1Info));
+
+    /* 填充消息头 */
+    MTC_CFG_IMSA_MSG_HDR(pstModem1Info, ID_MTC_IMSA_MODEM1_INFO_IND);
+
+    /* 填写Modem1的信息 */
+    pstModem1Info->enPowerState = MTC_GetModemPowerState(MODEM_ID_1);
+
+    /* 发送消息 */
+    MTC_SEND_MSG(pstModem1Info);
+
+    return;
+}
+
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif
+
---
+++ drivers/vendor/hisi/modem/taf/gu/src/ccore/src/SDC/Inc/TafSdcLib.h
@@ -0,0 +1,183 @@
+
+#ifndef _TAF_SDC_LIB_H_
+#define _TAF_SDC_LIB_H_
+/*****************************************************************************
+  1 其他头文件包含
+*****************************************************************************/
+#include  "TafSdcCtx.h"
+
+#include "TafOamInterface.h"
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif
+
+
+#pragma pack(4)
+
+/*****************************************************************************
+  2 宏定义
+*****************************************************************************/
+#define PRE_SECOND_TO_TICK                      (100)
+
+/*****************************************************************************
+  3 枚举定义
+*****************************************************************************/
+
+
+/*****************************************************************************
+  4 全局变量声明
+*****************************************************************************/
+#define TAF_SDC_GET_CURC_RPT_CTRL_STATUS_MAP_TBL_PTR()    (&g_aenTafSdcCurcRptCmdTable[0])
+#define TAF_SDC_GET_CURC_RPT_CTRL_STATUS_MAP_TBL_SIZE()   (sizeof(g_aenTafSdcCurcRptCmdTable)/sizeof(TAF_SDC_RPT_CMD_INDEX_ENUM_UINT8))
+
+#define TAF_SDC_GET_UNSOLICITED_RPT_CTRL_STATUS_MAP_TBL_PTR()    (&g_aenTafSdcUnsolicitedRptCmdTable[0])
+#define TAF_SDC_GET_UNSOLICITED_RPT_CTRL_STATUS_MAP_TBL_SIZE()   (sizeof(g_aenTafSdcUnsolicitedRptCmdTable)/sizeof(TAF_SDC_RPT_CMD_INDEX_ENUM_UINT8))
+
+#define TAF_SDC_MIN(n1,n2)                                  (((n1) > (n2)) ? (n2):(n1))
+
+
+/*****************************************************************************
+  5 消息头定义
+*****************************************************************************/
+
+
+/*****************************************************************************
+  6 消息定义
+*****************************************************************************/
+
+
+/*****************************************************************************
+  7 STRUCT定义
+*****************************************************************************/
+
+/*****************************************************************************
+ 结构名    : TAF_SDC_LOG_NETWORK_AND_APPNETWORK_CTX_INFO_STRU
+ 结构说明  :TAF的g_stTafSdcCtx中的NETWORK/APPNETWORK信息可维可测
+
+ 修改记录  :
+  1.日    期   : 2015年02月11日
+    作    者   : l00305157
+    修改内容   : Service_State_Optimize_PhaseII 项目修改
+*****************************************************************************/
+typedef struct
+{
+    MSG_HEADER_STRU                     stMsgHeader;/* 消息头                                   */ /*_H2ASN_Skip*/
+    TAF_SDC_NETWORK_INFO_STRU           stNetworkInfo;                          
+    TAF_SDC_APPNETWORK_INFO             stAppNetworkInfo;
+}TAF_SDC_LOG_NETWORK_AND_APPNETWORK_CTX_INFO_STRU;
+
+/*****************************************************************************
+  8 UNION定义
+*****************************************************************************/
+
+
+/*****************************************************************************
+  9 OTHERS定义
+*****************************************************************************/
+
+
+/*****************************************************************************
+  10 函数声明
+*****************************************************************************/
+extern VOS_UINT32 OM_TraceMsgHook(VOS_VOID* pMsg);
+
+VOS_UINT32  TAF_SDC_IsPlatformRatSupported(
+    TAF_SDC_PLATFORM_RAT_TYPE_ENUM_UINT8                    enSpecRat
+);
+
+VOS_UINT32  TAF_SDC_IsPlatformSupportLte( VOS_VOID );
+
+VOS_UINT32  TAF_SDC_IsPlatformSupportUtran( VOS_VOID );
+VOS_UINT32  TAF_SDC_IsPlatformSupportUtranFdd( VOS_VOID );
+#if (FEATURE_ON == FEATURE_UE_MODE_TDS)
+VOS_UINT32  TAF_SDC_IsPlatformSupportUtranTdd( VOS_VOID );
+#endif
+
+VOS_UINT32  TAF_SDC_IsPlatformSupportGsm( VOS_VOID );
+
+VOS_UINT32 TAF_SDC_CheckRptCmdStatus(
+    VOS_UINT8                                  *pRptCfg,
+    TAF_SDC_CMD_RPT_CTRL_TYPE_ENUM_UINT8        enRptCtrlType,
+    TAF_SDC_RPT_CMD_INDEX_ENUM_UINT8            enRptCmdIndex
+);
+VOS_UINT32 TAF_SDC_UpdateRptCmdStatus(
+    TAF_SDC_RPT_CMD_INDEX_ENUM_UINT8            enRptCmdIndex,
+    VOS_UINT32                                  ulRptCmdStatus
+);
+
+VOS_UINT32 TAF_SDC_ConvertOamSendPid(
+    VOS_UINT32                          ulConvertedSendPid
+);
+
+VOS_UINT32 TAF_SDC_GetCsSrvExistFlg(VOS_VOID);
+
+
+
+VOS_UINT32 TAF_SDC_GetImsSupportFlag(VOS_VOID);
+
+#if (FEATURE_ON == FEATURE_PTM)
+VOS_UINT32 TAF_SDC_IsErrLogNeedRecord(VOS_UINT16 usLevel);
+#endif
+
+VOS_UINT32  TAF_CalcTime(
+                VOS_UINT32  ulFirTime,
+                VOS_UINT32  ulSecTime,
+                VOS_UINT32 *pulTotalTime
+);
+
+VOS_UINT32 TAF_SDC_IsUsimStausValid(VOS_VOID);
+
+
+#if (FEATURE_ON == FEATURE_IMS)
+VOS_UINT32 TAF_SDC_IsImsVoiceSupportedInUE(VOS_VOID);
+VOS_UINT32 TAF_SDC_IsImsVoiceSupportedInNW(VOS_VOID);
+#endif
+
+VOS_VOID TAF_SDC_PlmnId2Bcd(
+    TAF_SDC_PLMN_ID_STRU               *pstPlmnId
+);
+
+VOS_UINT32 TAF_SDC_IsConfigCLInterWork(VOS_VOID);
+
+VOS_UINT32 TAF_SDC_IsLteOnlyMode(
+    TAF_SDC_RAT_PRIO_STRU              *pstRatOrder
+);
+
+VOS_UINT32 TAF_SDC_IsCsNormalService(VOS_VOID);
+
+VOS_UINT32 TAF_SDC_IsImsSrvExist(VOID);
+
+
+VOS_UINT8 TAF_SDC_IsCurrentNormalServiceStatus(void);
+
+VOS_UINT8 TAF_SDC_IsCurrentNormalRegStatus(void);
+
+VOS_VOID TAF_MMA_UpdateRoamFlag(
+    TAF_SDC_REG_STATUS_ENUM_UINT8      enRegState
+);
+
+VOS_UINT32 TAF_SDC_IsAppSepcDomainRegStaChg(
+    TAF_SDC_SERVICE_DOMAIN_ENUM_UINT8   enCnDomainId,
+    TAF_SDC_REG_STATUS_ENUM_UINT8       enRegState
+);
+
+
+#if (VOS_OS_VER == VOS_WIN32)
+#pragma pack()
+#else
+#pragma pack(0)
+#endif
+
+
+
+
+#ifdef __cplusplus
+    #if __cplusplus
+        }
+    #endif
+#endif
+
+#endif /* end of TafSdcLib.h */
